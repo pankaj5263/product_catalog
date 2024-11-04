7bafc146912e4922d6b9653d8936af90dbe817c6
Pankaj Sharma <pankaj.sharma@pankajsharmass-493.successivetech.com>
2024-11-04 11:07:54 +0530

    fixed product list page

--- app/components/ProductList.js
+++ app/components/ProductList.js
@@ -13,7 +13,7 @@ export default function ProductsPage() {
 
   useEffect(() => {
     const fetchProducts = async () => {
-      const response = await fetch(`http://localhost:3000/api/products`);
+      const response = await fetch(`/api/products`);
       const data = await response.json();
       console.log("products---", data);
       setProducts(data);
